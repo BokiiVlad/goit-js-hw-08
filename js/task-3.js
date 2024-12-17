1975454695663c7b925fb670c7c2d8111a511c5f
Vlad Bokii <bokiyvladik10@gmail.com>
2024-12-17 20:42:25 +0200

    hw7

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,8 @@
-class StringBuilder {
-    #value;
-    constructor(initialValue) {
-        this.#value = initialValue;
-    }
-    getValue() {
-        return this.#value
-    }
-    padEnd(str) {
-        this.#value = this.#value + str;
-    }
-    padStart(str) {
-        this.#value = str + this.#value;
-    }
-    padBoth(str) {
-        this.#value = str + this.#value + str;
-    }
-}
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+const findInput = document.querySelector("#name-input");
+const findSpan = document.querySelector("#name-output");
+findInput.addEventListener("input", event => {
+    const correctName = findInput.value.trim();
+    if (correctName) {
+        findSpan.textContent = correctName;
+    } else { findSpan.textContent = "Anonymous" }
+});
