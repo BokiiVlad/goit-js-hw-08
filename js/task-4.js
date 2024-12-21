6c2f3a765a8d23e60f8b6b679eb85e69b90913a8
Vlad Bokii <bokiyvladik10@gmail.com>
2024-12-21 22:32:32 +0200

    hw7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
-
+const itemForm = document.querySelector(".login-form");
+itemForm.addEventListener("submit", event => {
+    event.preventDefault();
+    if (!itemForm.elements.email.value || !itemForm.elements.password.value) {
+        alert('All form fields must be filled in');
+        return;
+    }
+    const formData = {
+        [itemForm.elements.email.name]: itemForm.elements.email.value,
+        [itemForm.elements.password.name]: itemForm.elements.password.value,
+    }
+    console.log(formData);
+    itemForm.reset();
+})
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const itemCreate = document.querySelector("[data-create]");
+const itemDestroy = document.querySelector("[data-destroy]");
+const itemInput = document.querySelector("#controls input");
+const elementArea = document.querySelector("#boxes");
+
+itemCreate.addEventListener("click", event => {
+  const trueValue = Number(itemInput.value);
+  if (trueValue < 100 && trueValue > 0) {
+    createBox(trueValue);
+    itemInput.value = "";
+  } else { return; };
+});
+
+itemDestroy.addEventListener("click", event => {
+  elementArea.innerHTML = "";
+})
+
+const createBox = amount => {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const boxItem = document.createElement("div");
+    boxItem.style.height = `${size}px`;
+    boxItem.style.width = `${size}px`;
+    boxItem.style.backgroundColor = getRandomHexColor();
+    boxItem.classList.add("box-item");
+    size += 10;
+    elementArea.prepend(boxItem);
+  }
+}
+
+elementArea.classList.add("box-div");
+itemDestroy.classList.add("btn-destroy");
+itemCreate.classList.add("btn-create");
