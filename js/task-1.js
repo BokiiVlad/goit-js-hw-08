bb61be9c2ff127f63c56631a5c747cf396b3842e
Vlad Bokii <bokiyvladik10@gmail.com>
2024-12-28 21:22:59 +0200

    hw8

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const itemCategories = document.querySelectorAll("#categories .item");
-
-console.log(`Number of categories ${itemCategories.length}`);
-itemCategories.forEach(item => {
-    const nameCategory = item.querySelector("h2").textContent;
-    console.log(`Category: ${nameCategory}`);
-    const numberElements = item.querySelectorAll("ul li");
-    console.log(`Elements: ${numberElements.length}`);
-})
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const findInput = document.querySelector("#name-input");
-const findSpan = document.querySelector("#name-output");
-findInput.addEventListener("input", event => {
-    const correctName = findInput.value.trim();
-    if (correctName) {
-        findSpan.textContent = correctName;
-    } else { findSpan.textContent = "Anonymous" }
-});
